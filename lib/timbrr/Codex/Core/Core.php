a6f9e3e589866246b1918ae27fa12c338ab7d713
Comkid <comkidwizzer3@hotmail.com>
2012-05-23 18:45:11 +1000

    Changed spaces to tabs

--- lib/timbrr/Codex/Core/Core.php
+++ lib/timbrr/Codex/Core/Core.php
@@ -4,33 +4,33 @@ namespace timbrr\codex\Core;
 
 class Core
 {
-    private static $version = '1.0.0-dev';
-    protected static $config = array(), $objects = array();
+	private static $version = '1.0.0-dev';
+	protected static $config = array(), $objects = array();
 
-    public static function getVersion()
-    {
-        return self::$version;
-    }
+	public static function getVersion()
+	{
+		return self::$version;
+	}
 
-    public static function setConfig($config_name, $config_value)
-    {
-        self::$config[$config_name] = $config_value;
-    }
+	public static function setConfig($config_name, $config_value)
+	{
+		self::$config[$config_name] = $config_value;
+	}
 
-    public static function getConfig($config_name)
-    {
-        return (!isset(self::$config[$config_name])) ? NULL : self::$config[$config_name];
-    }
+	public static function getConfig($config_name)
+	{
+		return (!isset(self::$config[$config_name])) ? NULL : self::$config[$config_name];
+	}
 
-    public static function setObject($slot, $object)
-    {
-        self::$objects[(string) $slot] = $object;
+	public static function setObject($slot, $object)
+	{
+		self::$objects[(string) $slot] = $object;
 
-        return $object;
-    }
+		return $object;
+	}
 
-    public static function getObject($slot)
-    {
-        return (!isset(self::$objects[(string) $slot])) ? NULL : self::$objects[(string) $slot];
-    }
+	public static function getObject($slot)
+	{
+		return (!isset(self::$objects[(string) $slot])) ? NULL : self::$objects[(string) $slot];
+	}
 }
